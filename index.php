ea2a3d27c874d3b4c10af35590acbd9fff1549e6
Mamta-logiciel <mamta.rani@logicielsolutions.co.in>
2016-04-14 16:25:34 +0530

    Header and Home page

--- index.php
+++ index.php
@@ -2,7 +2,55 @@
 <?php include('header.php'); ?>
 <!-- End Header Part -->
 
-	<div class="container">
+	<!-- Top Banner -->
+	<div class="top-banner">
+		<div class="container">
+			<div class="banner-content">
+				<h1>Welcome to <span>Avisyn</span></h1>
+				<p>We keep your drug discovery and development programs moving with expert insourced teams. </p>
+			</div>
+		</div>
+	</div>
+	<!-- /Top Banner -->
+
+	<div class="main-section">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-6">
+					<div class="content-section">
+						<h3>What is Insourcing?</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. </p>
+						<a href="#" class="btn btn-custom-blue btn-lg">Learn More</a>
+					</div>
+				</div>
+				<div class="col-sm-6">
+					<img src="images/insourcing.png" alt="Insourcing">
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="cols-section">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4">
+			      	<div class="text-center"><img src="//cdn.shopify.com/s/files/1/0691/5403/t/121/assets/components-icon.svg?3719080539427352740" class="homepage-grid-icon"></div>
+			      	<h5><strong>Components and examples</strong></h5>
+			      	<p class="text-muted">Each theme features new components built to the same level of quality as Bootstrap and highlighted with several example pages.</p>
+			    </div>
+				<div class="col-md-4">
+			      	<div class="text-center"><img src="//cdn.shopify.com/s/files/1/0691/5403/t/121/assets/components-icon.svg?3719080539427352740" class="homepage-grid-icon"></div>
+			      	<h5><strong>Components and examples</strong></h5>
+			      	<p class="text-muted">Each theme features new components built to the same level of quality as Bootstrap and highlighted with several example pages.</p>
+			    </div>
+			    <div class="col-md-4">
+			      	<div class="text-center"><img src="//cdn.shopify.com/s/files/1/0691/5403/t/121/assets/components-icon.svg?3719080539427352740" class="homepage-grid-icon"></div>
+			      	<h5><strong>Components and examples</strong></h5>
+			      	<p class="text-muted">Each theme features new components built to the same level of quality as Bootstrap and highlighted with several example pages.</p>
+			    </div>
+			</div>
+		</div>
 	</div>
 
 <!-- Footer Part -->
