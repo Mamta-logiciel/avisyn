6e5fcd34cf76f34687f4e7abd9bd717708624fb3
tanu-logiciel <tanu.baweja@logicielsolutions.co.in>
2016-04-14 19:13:23 +0530

    contact and about page create

---
+++ about.php
@@ -0,0 +1,82 @@
+<?php include_once 'header.php'; ?>
+
+<div class="main-wrap">
+	<div class="container">
+
+	<!-- Sticky buttons section starts -->
+		<section class="sticky-btn-wrap">
+			<a href="">
+				<div class="sticky-btn careers">
+					start a career with us
+				</div>
+			</a>
+			<a href="">
+				<div class="sticky-btn project">
+					start a project with us
+				</div>
+			</a>
+		</section>
+		<!-- Sticky buttons section ends -->
+		
+		<!-- Page heading section starts -->
+		<section class="heading-wrap">
+			<h1 class="title">about avisyn</h1>
+			<p class="tag-line">Inovative scientific staffing solutions customized for the needs of drug discovery and development.</p>
+		</section>
+		<!-- Page heading section ends -->	
+	</div>
+</div>
+		<!-- Form section starts -->
+<div class="content-container">
+	<div class="container">
+		<section class="content-wrap about-container">
+			<div class="who-avisyn">
+				<div class="row">
+					<div class="col-md-6 about-avisyn-outer">
+						<h2 class="title">who is avisyn?</h2>
+						<p>While insourcing is not a new concept, it’s not that widely used in the drug discovery and development industry. And yet it seems ideally suited to a business where you need different specialized experts at each stage of the pipeline. Why go through the lengthy process of finding and hiring a team of scientists only to have to lay them off a year later when that phase of the project is complete? It was in part my own personal experience going through that process that inspired me and my partners to start AviSyn.</p>
+					</div>
+					<div class="col-md-6 founder-img-outer">
+						<img src="images/avisyn.png" class="img-responsive founder-img">
+					</div>
+				</div>
+			</div>
+			<div class="about-content-wrap">
+				<div class="row superior-benefits-content">
+					<div class="col-md-5">
+						<h2 class="title superior-benefits">“The superior benefits of insourcing versus outsourcing—faster communication and greater collaboration“</h2>
+					</div>
+					<div class="col-md-7">
+						<p>Many years back, I was a scientist at AstraZeneca responsible for review and oversight of our outsourcing strategies and CRO partners. We faced an urgent resourcing gap in our chemistry department, but we had a head-count freeze. After considering multiple options, we decided to insource fifteen synthetic chemists—20% of our total count—because of the speed and flexibility of the process. These chemists had outstanding qualifications and I was able to see first-hand the superior benefits of insourcing versus outsourcing—faster communication and greater collaboration because we were all there in the same place.</p>
+					</div>
+				</div>
+				<p class="other-content">
+					Then the site closed and all positions were terminated. I started my own independent pharma research company and quickly came to recognize the challenges in finding and retaining the highest quality researchers of the caliber that we had working in a big pharma company. There is no substitute for co-located, expert chemistry talent for fast-moving innovative projects.
+				</p>
+				<p class="other-content">
+					At the same time, I saw many of my outstanding former colleagues remaining under-employed because R&D companies were reducing their permanent staff and moving increasingly to external CROs. From experience, we knew that the cost for high-quality CRO resourcing was rapidly increasing and soon would no longer justify the accompanying reduction in speed and innovation.
+				</p>
+				<p class="other-content">
+					It was the alignment of all these experiences that led us to start AviSyn.
+				</p>
+				<p class="other-content">
+					For companies, we offer teams of experienced scientists deployed directly by your management group at your site—you get all the benefits of an experienced CRO with the speed and innovation of an in-house team.
+				</p>
+				<p class="other-content">
+					For scientists, we combine the rewards of growing a scientific career with top-notch pharma and biotech companies with the security of knowing that when your project is done, you’ll have a new assignment soon.
+				</p>
+				<p class="other-content">
+					At AviSyn, we’re re-formulating the way drug discovery and development gets done.
+				</p>
+				<p class="other-content"><span class="author">– Jeffrey Albert, PhD.,</span> <span class="founder">AviSyn Founder</span></p>
+
+				<div class="contact-us-link">
+				<p><span class="contact">Contact us</span> to learn more.</p>
+				</div>
+			</div>
+		</section>
+		<!-- Form section ends -->
+	</div>
+</div>
+
+<?php include_once 'footer.php'; ?>
---
+++ contact-project.php
@@ -0,0 +1,39 @@
+<?php include_once 'header.php'; ?>
+
+<div class="main-wrap">
+	<div class="container">
+		<!-- Page heading section starts -->
+		<section class="heading-wrap">
+			<h1 class="title">start a project with us</h1>
+			<p class="tag-line">Tell us about your drug discovery and development scientific needs.</p>
+		</section>
+		<!-- Page heading section ends -->
+	</div>
+</div>
+<div class="content-container">
+	<div class="container">
+		<!-- Form section starts -->
+		<section class="content-wrap contact-form">
+			<form>
+				<p class="required-instruction">Fields marked with an <span class="required">*</span> are required</p>
+				<div class="form-group">
+					<label for="name">Name <span class="required">*</span></label>
+					<input type="text" class="text-box form-control" id="name">
+				</div>
+				<div class="form-group">
+					<label for="email">Email <span class="required">*</span></label>
+					<input type="email" class="text-box form-control" id="email">
+				</div>
+				<div class="form-group">
+					<label for="message">Message <span class="required">*</span></label>
+					<textarea class="text-box form-control" id="message"></textarea>
+				</div>
+				<p>200 word(s) left</p>
+				<button type="submit" class="btn btn-default submit">Submit</button>
+			</form>
+		</section>
+		<!-- Form section ends -->
+	</div>
+</div>
+
+<?php include_once 'footer.php'; ?>
