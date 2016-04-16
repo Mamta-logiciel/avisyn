14e33c6eed5c42fff878ccb5b2a802a8f620d985
tanu-logiciel <tanu.baweja@logicielsolutions.co.in>
2016-04-16 15:46:26 +0530

    code with spacing manage

--- services.php
+++ services.php
@@ -28,14 +28,14 @@
 			<!-- Section What is Insourcing -->
 			<div class="who-avisyn insourcing-wrap">
 				<div class="row">
-					<div class="col-md-6 about-avisyn-outer">
+					<div class="col-md-6 about-avisyn-outer right-padding">
 						<h2 class="title">what is insourcing?</h2>
 						<p>Insourcing is an innovative scientific staffing solution that is ideally suited for the changing stages of drug discovery and development programs.</p>
 						<p>Combining the flexibility and expertise of outsourcing to a CRO with the immediate communication, collaboration, and IP security possible with an in-house team, insourcing lets companies hire expert teams of scientists for defined project stages. Simply contact us with your project needs, and within weeks we’ll have a team up and running at your site.</p>
 						<hr class="text-with-border">
 						<p class="find-out-text"><span>Find out how</span> one scientist’s insourcing experience inspired him to start AviSyn. READ NOW</p>
 					</div>
-					<div class="col-md-6 founder-img-outer">
+					<div class="col-md-6 founder-img-outer left-padding">
 						<img src="images/insourcing-services.png" class="img-responsive founder-img">
 					</div>
 				</div>
